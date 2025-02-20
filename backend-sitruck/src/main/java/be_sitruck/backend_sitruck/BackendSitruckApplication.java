c52ca135ca52cb10e8b79066077e9e7172dbd426
Rifat Fauzan <rifat.fauzan8@gmail.com>
2025-02-20 22:30:19 +0700

    feat: Init Project

---
+++ backend-sitruck/src/main/java/be_sitruck/backend_sitruck/BackendSitruckApplication.java
@@ -0,0 +1,13 @@
+package be_sitruck.backend_sitruck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSitruckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSitruckApplication.class, args);
+	}
+
+}
