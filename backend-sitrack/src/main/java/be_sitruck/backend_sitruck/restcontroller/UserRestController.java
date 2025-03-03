1924f303d68688be489c34bfd479f311aa481c4f
Rif'At Fauzan <rifat.fauzan@ui.ac.id>
2025-03-03 19:36:13 +0700

    feat: Merge branch epic-02-manajemen-user to dev

--- backend-sitrack/src/main/java/be_sitruck/backend_sitruck/restcontroller/UserRestController.java
+++ backend-sitrack/src/main/java/be_sitruck/backend_sitruck/restcontroller/UserRestController.java
@@ -3,14 +3,19 @@ package be_sitruck.backend_sitruck.restcontroller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import be_sitruck.backend_sitruck.repository.UserDb;
 import be_sitruck.backend_sitruck.restdto.request.CreateUserRequestDTO;
+import be_sitruck.backend_sitruck.restdto.request.UpdateUserRequestDTO;
 import be_sitruck.backend_sitruck.restdto.response.BaseResponseDTO;
 import be_sitruck.backend_sitruck.restdto.response.CreateUserResponseDTO;
 import be_sitruck.backend_sitruck.restservice.UserRestService;
@@ -60,5 +65,74 @@ public class UserRestController {
 
         return new ResponseEntity<>(baseResponseDTO, HttpStatus.OK);
 
+    }
+
+    @GetMapping("/detail")
+    public ResponseEntity<?> getUserById(@RequestParam("id") Long id) {
+        var baseResponseDTO = new BaseResponseDTO<CreateUserResponseDTO>();
+        try {
+            CreateUserResponseDTO userResponseDTO = userRestService.getUserById(id);
+            baseResponseDTO.setStatus(HttpStatus.OK.value());
+            baseResponseDTO.setMessage("User berhasil ditemukan!");
+            baseResponseDTO.setTimestamp(new Date());
+            baseResponseDTO.setData(userResponseDTO);
+            return ResponseEntity.ok(baseResponseDTO);
+
+        } catch (Exception e) {
+            baseResponseDTO.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            baseResponseDTO.setMessage(e.getMessage());
+            baseResponseDTO.setTimestamp(new Date());
+            baseResponseDTO.setData(null);
+            
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                               .body(baseResponseDTO);
+        }   
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<BaseResponseDTO<CreateUserResponseDTO>> updateUser(
+            @RequestParam("id") Long id,
+            @RequestBody UpdateUserRequestDTO requestDTO) {
+        try {
+            CreateUserResponseDTO updatedUser = userRestService.updateUser(id, requestDTO);
+            
+            var baseResponseDTO = new BaseResponseDTO<CreateUserResponseDTO>();
+            baseResponseDTO.setStatus(HttpStatus.OK.value());
+            baseResponseDTO.setData(updatedUser);
+            baseResponseDTO.setMessage("User dengan ID " + id + " berhasil diperbarui");
+            baseResponseDTO.setTimestamp(new Date());
+            return ResponseEntity.ok(baseResponseDTO);
+        } catch (NoSuchElementException e) {
+            var baseResponseDTO = new BaseResponseDTO<CreateUserResponseDTO>();
+            baseResponseDTO.setStatus(HttpStatus.NOT_FOUND.value());
+            baseResponseDTO.setMessage(e.getMessage());
+            baseResponseDTO.setTimestamp(new Date());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(baseResponseDTO);
+        } catch (IllegalArgumentException e) {
+            var baseResponseDTO = new BaseResponseDTO<CreateUserResponseDTO>();
+            baseResponseDTO.setStatus(HttpStatus.BAD_REQUEST.value());
+            baseResponseDTO.setMessage(e.getMessage());
+            baseResponseDTO.setTimestamp(new Date());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(baseResponseDTO);
+        }
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deleteUser(@RequestParam("id") Long id) {
+        var baseResponseDTO = new BaseResponseDTO<String>();
+        try {
+            userRestService.deleteUserById(id);
+            baseResponseDTO.setStatus(HttpStatus.OK.value());
+            baseResponseDTO.setMessage("User berhasil dihapus!");
+            baseResponseDTO.setTimestamp(new Date());
+            baseResponseDTO.setData(null);
+            return ResponseEntity.ok(baseResponseDTO);
+        } catch (Exception e) {
+            baseResponseDTO.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            baseResponseDTO.setMessage(e.getMessage());
+            baseResponseDTO.setTimestamp(new Date());
+            baseResponseDTO.setData(null);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(baseResponseDTO);
+        }
     }
 }
