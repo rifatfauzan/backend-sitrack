a9c53462b7516aaee301e86897738c2888b16dc9
azmifal <kunazmifalah@gmail.com>
2025-04-07 20:11:27 +0700

    fix: sprint 1 revision

--- backend-sitrack/src/main/java/be_sitruck/backend_sitruck/security/WebSecurityConfig.java
+++ backend-sitrack/src/main/java/be_sitruck/backend_sitruck/security/WebSecurityConfig.java
@@ -122,6 +122,7 @@ public class WebSecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOriginPattern("https://sitrack.up.railway.app");
+        config.addAllowedOriginPattern("http://localhost:4173");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
